2a12f9c64eebb2e16cd36d0160663feaf413f9b5
Adriel2390 <dquevedo@insgabrielamistral.cat>
2025-04-08 08:27:12 +0200

    Update Botiga_analitycs.py

---
+++ ABT_Grup5/Botiga_analitycs.py
@@ -0,0 +1,17 @@
+import csv
+
+def menu():
+    while True:
+        print(" ___________________________________________________________________")
+        print("|----------------Informacion del Mercado de W Tech------------------|")
+        print("|___________________________________________________________________|")
+        print("|------|1. Mostrar facturacio mensual (amb i sense IVA)--------|----|")
+        print("|------|2. Mostrar estock disponible---------------------------|----|")
+        print("|------|3. Mostrar els 3 primers products amb mes facturacio---|----|")
+        print("|------|4. Sortir----------------------------------------------|----|")
+
+        opcio = int(input("tria una opció de 1 al 4"))
+        if opcio == 1: print
+        elif opcio == 2: print
+        elif opcio == 3: print
+        elif opcio == 4: break
