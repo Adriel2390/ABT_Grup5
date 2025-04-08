fc35d3ebcf05d6bc4348ffeb00e52c34e5b265c4
Adriel2390 <dquevedo@insgabrielamistral.cat>
2025-04-08 11:15:04 +0200

    Update Botiga_analitycs.py

--- ABT_Grup5/Botiga_analitycs.py
+++ ABT_Grup5/Botiga_analitycs.py
@@ -1,5 +1,37 @@
 import csv
 
+dades = ""
+add = []
+
+with open('dades_botiga.csv', newline="") as a:
+    data = csv.reader(a, delimiter=",")
+    dades = list(data)
+
+#print(dades)
+
+def orden(dades):
+    for i in range(1, len(dades)):
+        for j in range(len(dades[i])):
+            print(dades[0][j] + ": " + dades[i][j])
+        print("")
+
+#orden(dades)
+
+def add(dades):
+    for i in range(1, len(dades)):
+        sum = 0
+        med = 0
+        print(dades[0][0] + ": " + dades[i][0])
+
+        for j in range(1, len(dades[i])):
+            print(dades[0][j] + ": " + dades[i][j])
+        print("")
+
+#add(dades)
+
+
+
+
 def menu():
     while True:
         print(" ___________________________________________________________________")
@@ -10,8 +42,10 @@ def menu():
         print("|------|3. Mostrar els 3 primers products amb mes facturacio---|----|")
         print("|------|4. Sortir----------------------------------------------|----|")
 
-        opcio = int(input("tria una opció de 1 al 4"))
+        opcio = int(input("tria una opció de 1 al 4: "))
         if opcio == 1: print
         elif opcio == 2: print
         elif opcio == 3: print
         elif opcio == 4: break
+
+#menu()
