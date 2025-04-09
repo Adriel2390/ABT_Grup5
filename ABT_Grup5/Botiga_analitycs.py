c366e84b4095f03263c7a7618cda7ba5de6edc52
Adriel2390 <dquevedo@insgabrielamistral.cat>
2025-04-09 08:07:57 +0200

    Update Botiga_analitycs.py

--- ABT_Grup5/Botiga_analitycs.py
+++ ABT_Grup5/Botiga_analitycs.py
@@ -1,40 +1,28 @@
 import csv
 
-dades = ""
-add = []
+def facturacio():
+    Factuacio_senseIVA = 0
+    Factuacio_ambIVA = 0
 
-with open('dades_botiga.csv', newline="") as a:
-    data = csv.reader(a, delimiter=",")
-    dades = list(data)
+    arxiu_csv = "dades_botiga.csv"
+    with open(arxiu_csv, newline='', encoding='utf-8') as arxiu:
+        lector_csv = csv.DictReader(arxiu)
+        
+        for i in lector_csv:
+            quantitat_venuda = int(i['Quantitat_Venuda'])
+            preu = float(i['Preu_Unitari'])
+            IVA = float(i['IVA'])
 
-#print(dades)
+            preu_amb_IVA = preu * (1 + IVA / 100)
+            Factuacio_senseIVA += quantitat_venuda * preu
+            Factuacio_ambIVA += quantitat_venuda * preu_amb_IVA
 
-def orden(dades):
-    for i in range(1, len(dades)):
-        for j in range(len(dades[i])):
-            print(dades[0][j] + ": " + dades[i][j])
-        print("")
-
-#orden(dades)
-
-def add(dades):
-    for i in range(1, len(dades)):
-        sum = 0
-        med = 0
-        print(dades[0][0] + ": " + dades[i][0])
-
-        for j in range(1, len(dades[i])):
-            print(dades[0][j] + ": " + dades[i][j])
-        print("")
-
-#add(dades)
-
-
-
+    print(f"La facturació sense IVA és: {Factuacio_senseIVA:.2f}€")
+    print(f"La facturació amb IVA és: {Factuacio_ambIVA:.2f}€")
 
 def menu():
     while True:
-        print(" ___________________________________________________________________")
+        print("\n ___________________________________________________________________")
         print("|----------------Informacion del Mercado de W Tech------------------|")
         print("|___________________________________________________________________|")
         print("|------|1. Mostrar facturacio mensual (amb i sense IVA)--------|----|")
@@ -43,9 +31,10 @@ def menu():
         print("|------|4. Sortir----------------------------------------------|----|")
 
         opcio = int(input("tria una opció de 1 al 4: "))
-        if opcio == 1: print
+        if opcio == 1: facturacio()
         elif opcio == 2: print
         elif opcio == 3: print
         elif opcio == 4: break
+        else: print("\nOpció no vàlida. Torna-ho a intentar.\n")
 
-#menu()
+menu()
